dcb86282506f15c851e6e1e9f4006bc08d64539a
Elvis Arabadjiyski <elvis@ever.co>
2019-05-25 15:21:24 +0300

    .

--- src/server/routes/products.js
+++ src/server/routes/products.js
@@ -1,17 +1,21 @@
-const express = require("express");
-const Product = require("../models/Product");
+const express = require('express');
+const Product = require('../models/Product');
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-  try {
-    const data = await Product.find().exec();
-    res.setHeader('Content-Type', 'application/json');
-    res.json(JSON.stringify(data));
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-  res.end();
+router.get('/', async (req, res) => {
+    try {
+        const data = await Product.find().exec();
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(`
+            <h1>Hello this is products component!</h1>
+            <p>Data Length: ${data.length}</p>
+            <p>Data: ${data}</p>
+        `);
+        res.end();
+    } catch (err) {
+        res.status(500).send({ error: err.message });
+    }
 });
 
 module.exports = router;
