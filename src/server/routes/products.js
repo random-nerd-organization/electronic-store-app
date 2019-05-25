c51c8d756d2637beffc1e183a9b60db374b4eb3d
Elvis Arabadjiyski <elvis@ever.co>
2019-05-25 14:34:43 +0300

    .

--- src/server/routes/products.js
+++ src/server/routes/products.js
@@ -4,12 +4,16 @@ const Product = require("../models/Product");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  try {
-    const data = await Product.find().exec();
-    res.status(200).send(data);
-  } catch (err) {
-    res.status(500).send({ error: err.message });
-  }
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write("<h1>HELLO CART COMPONENTPAGE</h1>");
+  res.end();
+
+  // try {
+  //   const data = await Product.find().exec();
+  //   res.status(200).send(data);
+  // } catch (err) {
+  //   res.status(500).send({ error: err.message });
+  // }
 });
 
 module.exports = router;
